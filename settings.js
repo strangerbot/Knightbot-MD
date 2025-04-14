8428515f0cbf35195bf15ed775542ec40809bb73
strangerbot <lyfebotstranger@gmail.com>
2025-04-14 09:05:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: '𝐒𝐭𝐫𝐚𝐧𝐠𝐞𝐫 𖤍', // Your name
+  ownerNumber: '918076189643', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
